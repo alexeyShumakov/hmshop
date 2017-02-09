19a672c61bbeb69921ff61809b0ceb047acbcd22
alexeyShumakov <bobshaftoe@yandex.ru>
2017-02-10 03:38:25 +0500

    sidebar

--- app/javascript/packs/web/components/sidebar/Category.js
+++ app/javascript/packs/web/components/sidebar/Category.js
@@ -1,15 +1,47 @@
 import React from 'react';
+import ReactDOM, { findDOMNode } from 'react-dom';
 import { Link } from 'react-router';
+import Tether from 'tether';
+import _ from 'lodash';
 
-export default props => {
-  function fetchCategory() {
-    props.fetchCategory(props.category.get('id'))
+export default class Category extends React.Component {
+
+  constructor(props) {
+    super(props);
+    _.bindAll(this, 'fetchCategory');
   }
-  return(
-    <li>
-      <Link to={`/categories/${props.category.get('id')}`} onClick={fetchCategory}>
-        <i className="nav__icon fa fa-heart-o fa-lg"></i>
-      </Link>
-    </li>
-  )
+
+  componentDidMount() {
+    let { tTarget, tElement } = this;
+    tTarget = findDOMNode(tTarget);
+    tElement = findDOMNode(tElement);
+    new Tether({
+      element: tElement,
+      target: tTarget,
+      attachment: 'top left',
+      targetAttachment: 'top right'
+    })
+  }
+
+  fetchCategory() {
+    let { fetchCategory, category } = this.props;
+    fetchCategory(category.get('id'));
+  }
+
+  render() {
+    let { category } = this.props;
+    return(
+      <li>
+        <Link
+          ref={(tTarget) => { this.tTarget = tTarget; }}
+          to={`/categories/${category.get('id')}`}
+          onClick={this.fetchCategory}>
+          <i className="nav__icon fa fa-heart-o fa-lg"></i>
+        </Link>
+        <div className='nav-sub-categories' ref={(tElement) => { this.tElement = tElement; }} >
+          hello
+        </div>
+      </li>
+    )
+  }
 }
