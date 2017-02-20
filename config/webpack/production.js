48f82df0a69412cb8b2f4213ae8a318b4cf45f75
alexey <bobshaftoe@yandex.ru>
2017-02-21 02:22:33 +0500

    copy uglify settings for wp

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -13,6 +13,17 @@ module.exports = merge(config, {
     new webpack.LoaderOptionsPlugin({
       minimize: true,
       debug: false
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+        beautify: false,
+        mangle: {
+            screw_ie8: true,
+            keep_fnames: true
+        },
+        compress: {
+            screw_ie8: true
+        },
+        comments: false
     })
   ]
 })
