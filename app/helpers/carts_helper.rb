3504c9931687784b82ddd04195c9438842f8fbf3
alexey <bobshaftoe@yandex.ru>
2017-03-01 23:29:11 +0500

    cart: fix n+1 request

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -1,6 +1,6 @@
 module CartsHelper
   def set_cart
-    @cart = Cart.find cookies[:cart_id]
+    @cart = Cart.includes(line_items: :product).find cookies[:cart_id]
   rescue ActiveRecord::RecordNotFound
     @cart = Cart.create
     cookies[:cart_id] = @cart.id
