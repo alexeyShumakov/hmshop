97a584e226e63a0a735cc6edfa4ea4d56c566f90
alexey <bobshaftoe@yandex.ru>
2017-03-03 19:15:14 +0500

    remove log

--- app/javascript/packs/web/store/store.js
+++ app/javascript/packs/web/store/store.js
@@ -18,7 +18,6 @@ if (process.env.NODE_ENV === `development`) {
 let shared = (typeof $sharedVariables === 'undefined') ? {} : $sharedVariables;
 let local = (typeof $localVariables === 'undefined') ? {} : $localVariables;
 let state = initialState.merge(fromJS(shared)).merge(fromJS(local));
-console.log(state);
 
 const store = createStore(appReducer, state, applyMiddleware(...middlewares) )
 
