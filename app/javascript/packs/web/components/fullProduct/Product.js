39bd07c47884ac3d93b94906c76ab79eb8ace537
alexey <bobshaftoe@yandex.ru>
2017-03-04 20:24:55 +0500

    fix full product category link

--- app/javascript/packs/web/components/fullProduct/Product.js
+++ app/javascript/packs/web/components/fullProduct/Product.js
@@ -10,31 +10,33 @@ export default (props, context) => {
     isLoading, currentPicture, fetchCategory } = props;
   return(
     <div className='container full-product'>
-      <Link
-        onClick={()=>{fetchCategory(product.getIn(['category', 'id']))}}
-        to={`/categories/${product.getIn(['category', 'id'])}`}>
-        <i className='fa fa-arrow-left'></i> {product.getIn(['category', 'title'])}
-      </Link>
       { !isLoading &&
-        <div className='row' >
-          <div className="column column-60">
-            <div className='full-product__big-picture-wrapper'>
-              <img src={currentPicture} className='full-product__big-picture'  alt=""/>
+        <div>
+          <Link
+            onClick={()=>{fetchCategory(product.getIn(['category', 'id']))}}
+            to={`/categories/${product.getIn(['category', 'id'])}`}>
+            <i className='fa fa-arrow-left'></i> {product.getIn(['category', 'title'])}
+          </Link>
+          <div className='row' >
+            <div className="column column-60">
+              <div className='full-product__big-picture-wrapper'>
+                <img src={currentPicture} className='full-product__big-picture'  alt=""/>
+              </div>
+              <Carousel
+                pictures={product.get('pictures')}
+                setCurrentPicture={setCurrentPicture}
+                currentPicture={currentPicture}
+              />
             </div>
-            <Carousel
-              pictures={product.get('pictures')}
-              setCurrentPicture={setCurrentPicture}
-              currentPicture={currentPicture}
-            />
-          </div>
 
-          <div className="column column-40">
-            <h3>{product.get('title')}</h3>
-            <h3>{product.get('price')} <i className='fa fa-rub'></i></h3>
-            <p>{product.get('description')}</p>
-            <div className="full-product__buttons">
-              <button onClick={() => {createLineItem(product.get('id'))}} className='button' >Добавить в корзину</button>
-              <button className='button button-outline' >Быстрая покупка</button>
+            <div className="column column-40">
+              <h3>{product.get('title')}</h3>
+              <h3>{product.get('price')} <i className='fa fa-rub'></i></h3>
+              <p>{product.get('description')}</p>
+              <div className="full-product__buttons">
+                <button onClick={() => {createLineItem(product.get('id'))}} className='button' >Добавить в корзину</button>
+                <button className='button button-outline' >Быстрая покупка</button>
+              </div>
             </div>
           </div>
         </div>
