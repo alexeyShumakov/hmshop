a13d6970c9069486f31f8bc815dcef1a2d6d405b
alexey <bobshaftoe@yandex.ru>
2017-03-09 01:45:56 +0500

    pictures: img optimization

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,6 +1,11 @@
 class Picture < ApplicationRecord
   has_and_belongs_to_many :products
-  has_attached_file :image, styles: { thumb: '235x235>', medium: '1000x650>' }
+  has_attached_file :image, styles: {
+    thumb: '235x235>', medium: '1000x650>'
+  }, convert_options: {
+    medium:  "-quality 60 -strip",
+    thumb:  "-quality 60 -strip"
+  }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
   def thumb_img
